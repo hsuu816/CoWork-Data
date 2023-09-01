4b269358698252a98eedb19f3f74ec0d9cb2daab
vul358 <b00303062@ntu.edu.tw>
2023-09-01 10:59:21 +0800

    add test

---
+++ test.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, make_response
+
+
+app = Flask(__name__)
+	
+
+@app.route('/get_headers', methods=['POST'])
+def get_headers():
+    # 取得 POST 請求的 headers
+    headers = request.headers
+    # 輸出 headers 資訊
+    return str(headers)
+
+
+if __name__ == "__main__":
+	app.run(debug=True, port=8000)
