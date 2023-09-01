264de2150b21d61d9632c8576e17dbfd300bd989
rubywu0604 <lksh20602@gmail.com>
2023-09-01 18:48:08 +0800

    feat: print connection

--- App/application.py
+++ App/application.py
@@ -3,6 +3,8 @@ from waitress import serve
 
 if __name__ == "__main__":
     if (app.env == "production"):
+        print('Prod. connect')
         serve(app, host='0.0.0.0', port = 3000)
     else:
+        print('Dev. connect')
         app.run('0.0.0.0', port = 5000)
